9ddeb354f178d3fbb49467c218109ff1ba180798
洋滨 <127976325+yangbin1322@users.noreply.github.com>
2025-12-13 05:43:33 +0800

    Merge remote-tracking branch 'origin/main'

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"log"
 
 	"github.com/wailsapp/wails/v3/pkg/application"
+	"github.com/wailsapp/wails/v3/pkg/events"
 )
 
 //go:embed all:frontend
@@ -31,6 +33,35 @@ func main() {
 	Service := NewMyService()
 	app.RegisterService(application.NewService(Service))
 
+	// 设置窗口关闭事件处理器
+
+	mainWindow.RegisterHook(events.Common.WindowClosing, func(e *application.WindowEvent) {
+		fmt.Println(e)
+		// 显示对话框，让用户选择操作
+		exitButton := &application.Button{
+			Label: "Yes",
+			Callback: func() {
+				app.Quit()
+			},
+		}
+
+		hideButton := &application.Button{
+			Label: "No",
+			Callback: func() {
+				e.Cancel() //取消窗口关闭事件
+				mainWindow.Hide()
+			},
+		}
+
+		app.Dialog.Question().
+			SetTitle("Xray 管理器").
+			SetMessage("请选择操作：\nYes = 退出程序\nNo = 最小化到托盘").
+			AddButtons([]*application.Button{exitButton, hideButton}).
+			SetDefaultButton(hideButton). // 默认最小化
+			SetCancelButton(hideButton).  // ESC 也最小化
+			Show()
+
+	})
 	// 创建系统托盘
 	systemTray := app.SystemTray.New()
 	if systemTray != nil {
